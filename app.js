23d956795d4d2d5e16c3d91a0a3d13e4b8e7ad78
Qasim Khan <56044350+Qasimkhan786@users.noreply.github.com>
2021-07-25 15:36:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+//Question#1
+// var count = 0;
+
+// var itemsArray =
+//     [
+//         { name: "juice", price: 50, quantity: "3" },
+//         { name: "cookie", price: 30, quantity: "9" },
+//         { name: "shirt", price: 880, quantity: "1" },
+//         { name: "pen", price: 100, quantity: "2" }
+//     ];
+// for (var i = 0; i < itemsArray.length; i++) {
+//     console.log("the " + itemsArray[i].name + " price is " + itemsArray[i].price)
+// }
+// for (var i = 0; i < itemsArray.length; i++) {
+
+//     count += itemsArray[i].price;
+
+// }
+// console.log("the total price of all items " + count);
+
+//Question#03
+// function Student (name,age,id,fatherName,batch)
+// {
+//   this.name=name;
+//   this.age=age;
+//   this.id=id;
+//   this.fatherName=fatherName;
+//   this.batch=batch;
+
+// };
+// var student1=new Student("Qasim",21,9832,"khan",7);
+// var student2=new Student("abdullah",23,104,"khalid",6);
+// var student3=new Student("moosa",13,11104,"akmal",5);
+// console.log(student1,student2,student3);
+
+
+
+//Question#02
+// var obj={
+
+//     firstName:'hanzala',
+//     lastName:"khalid",
+//     email:"abc@gmail.com",
+//     password:"1234",
+//     age:23,
+//     gender:'M',
+//     city:"karachi",
+//     country:"pakistan"
+//   };
+// console.log("firstName" in obj);
+// console.log("lastName" in obj);
+// console.log("age" in obj);
+// console.log("country" in obj); 
+
+//Question#04
